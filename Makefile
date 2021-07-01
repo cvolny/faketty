d7900a31a51dd36bb4c2a215a2cab6a1bf775063
Chris Volny <cvolny@duosecurity.com>
2021-07-01 10:05:21 -0400

    wip adding osx support

--- Makefile
+++ Makefile
@@ -1,17 +1,26 @@
 SRCFILE = preload.c
 LIBFILE = preload.so
+LIBFILE_OSX = preload.dylib
 TESTFILE = test.sh
 
 $(LIBFILE) : $(SRCFILE)
 	gcc $(SRCFILE) -o $(LIBFILE) -shared -fPIC
 
+$(LIBFILE_OSX) : $(SRCFILE)
+	gcc $(SRCFILE) -o $(LIBFILE_OSX) -dynamiclib
+
 compile : $(LIBFILE)
+
+compile-osx : $(LIBFILE_OSX)
 
 test : compile $(TESTFILE)
 	LD_PRELOAD=$(shell pwd)/$(LIBFILE) bash $(TESTFILE)
+
+test-osx : compile-osx $(TESTFILE)
+	DYLD_INSERT_LIBRARIES=$(shell pwd)/$(LIBFILE_OSX) bash $(TESTFILE)
 
 testneg : $(TESTFILE)
 	bash $(TESTFILE)
 
 clean :
-	rm $(LIBFILE)
+	rm -f $(LIBFILE) $(LIBFILE_OSX)
--- preload.c
+++ preload.c
@@ -1,6 +1,15 @@
-#include <stdlib.h>
+#define _GNU_SOURCE
+#define ONAME isatty
 
-int isatty(int fd) {
+#if __APPLE__
+    #define NAME _isatty
+    #include <unistd.h>
+#else
+    #define NAME isatty
+    #include <stdlib.h>
+#endif
+
+int NAME(int fd) {
     switch (fd) {
         case 0:
         case 1:
@@ -9,3 +18,11 @@ int isatty(int fd) {
     }
     return 0;
 }
+
+#if __APPLE__
+    #define DYLD_INTERPOSE(_replacment,_replacee) \
+        __attribute__((used)) static struct{ const void* replacment; const void* replacee; } _interpose_##_replacee \
+        __attribute__ ((section ("__DATA,__interpose"))) = { (const void*)(unsigned long)&_replacment, (const void*)(unsigned long)&_replacee };
+
+    DYLD_INTERPOSE(NAME, ONAME);
+#endif
